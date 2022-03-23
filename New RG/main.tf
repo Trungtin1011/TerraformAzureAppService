5c4632ce03afd00980eea863a4329c6f9383718c
Trungtin1011 <54236859+Trungtin1011@users.noreply.github.com>
2022-03-23 12:58:28 +0700

    Update main.tf
    Finish deploying WordPress

--- New RG/main.tf
+++ New RG/main.tf
@@ -1,20 +1,17 @@
+# Local Variables
 locals {
   rg_name = "RG_Group4_week3_20220321"
   location_2 = "Australia Central"
 }
 
 # Resource Group
-# --------------
-
 resource "azurerm_resource_group" "RG_Group4_week3_20220321" {
   name     = "RG_Group4_week3_20220321"
   location = var.location
   tags = var.tags
 }
 
-
 # App Service Plan
-# --------------
 resource "azurerm_app_service_plan" "group4-plan" {
   name                = "group4-apps-plan"
   location            = local.location_2
@@ -29,9 +26,7 @@ resource "azurerm_app_service_plan" "group4-plan" {
   }
 }
 
-
 # App Service 
-# --------------
 resource "azurerm_app_service" "group4-app" {
   name                    = "group4-app"
   location                = "Australia Central"
@@ -41,51 +36,41 @@ resource "azurerm_app_service" "group4-app" {
   #client_affinity_enabled = false
   tags                		= var.tags
 
- # site_config {
-    
-     
- #   }
+  source_control {
+    repo_url           = "https://github.com/WordPress/WordPress"
+    branch             = "master"
+    manual_integration = true
+    use_mercurial      = false
+  }
+}
 
- #   app_settings = {
- #     "SOME_KEY" = "some-value"
- #   }
+# Azure MySQL 
+resource "azurerm_mysql_server" "group4-sql" {
+ name                = "group4-mysqlserver"
+  location            = local.location_2
+  resource_group_name = azurerm_resource_group.RG_Group4_week3_20220321.name
 
- #   connection_string {
- #     name  = "Database"
- #     type  = "SQLServer"
- #     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
- # }
+  administrator_login          = "group04"
+  administrator_login_password = "Group@04"
+
+  sku_name   = "B_Gen5_1" #cheapest
+  storage_mb = 5120 #smallest
+  version    = "5.7"
+
+  auto_grow_enabled                 = false
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = false
+  #ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
 
-# Azure MySQL 
-# --------------
-#resource "azurerm_mysql_server" "SQL_Group4" {
-#  name                = "group4-week3-mysqlserver"
-#   location            = azurerm_resource_group.RG_Group4.location
-#   resource_group_name = azurerm_resource_group.RG_Group4.name
-
-#   administrator_login          = "group04"
-#   administrator_login_password = "Group@04"
-
-#   sku_name   = "B_Gen5_1" #cheapest
-#   storage_mb = 5120 #smallest
-#   version    = "5.7"
-
-#   auto_grow_enabled                 = true
-#   backup_retention_days             = 7
-#   geo_redundant_backup_enabled      = false
-#   infrastructure_encryption_enabled = false
-#   public_network_access_enabled     = true
-#   ssl_enforcement_enabled           = true
-#   ssl_minimal_tls_version_enforced  = "TLS1_2"
-# }
-
-
-# resource "azurerm_mysql_database" "wordpress" {
-#   name                = "Group4-week3-mysqlDB"
-#   resource_group_name = azurerm_resource_group.RG_Group4.name
-#   server_name         = azurerm_mysql_server.SQL_Group4.name
-#   charset             = "utf8"
-#   collation           = "utf8_unicode_ci"
-# }
+resource "azurerm_mysql_database" "group4-db" {
+  name                = "group4-mysqldb"
+  resource_group_name = azurerm_resource_group.RG_Group4_week3_20220321.name
+  server_name         = azurerm_mysql_server.group4-sql.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
