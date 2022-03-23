da7b2968d6d723d64649de6c8fa429264e8ac49a
Trungtin1011 <54236859+Trungtin1011@users.noreply.github.com>
2022-03-23 11:42:35 +0700

    Update main.tf

--- New RG/main.tf
+++ New RG/main.tf
@@ -1,3 +1,8 @@
+locals {
+  rg_name = "RG_Group4_week3_20220321"
+  location_2 = "Australia Central"
+}
+
 # Resource Group
 # --------------
 
@@ -10,68 +15,34 @@ resource "azurerm_resource_group" "RG_Group4_week3_20220321" {
 
 # App Service Plan
 # --------------
-resource "azurerm_app_service_plan" "Plan_Group4" {
-  name                = "Group4-week3-apps-plan"
-  location            = azurerm_resource_group.RG_Group4_week3_20220321.location
+resource "azurerm_app_service_plan" "group4-plan" {
+  name                = "group4-apps-plan"
+  location            = local.location_2
   resource_group_name = azurerm_resource_group.RG_Group4_week3_20220321.name
   tags                = var.tags
   kind                = "Linux"
   reserved            = true # must be true for Linux
 
   sku {
-    tier = "Basic"
-    size = "B2"
+    tier = "Standard"
+    size = "S1"
   }
 }
 
 
 # App Service 
 # --------------
-resource "azurerm_app_service" "App_Group4" {
-  name                    = "Group4-week3-app"
-  location                = azurerm_resource_group.RG_Group4_week3_20220321.location
+resource "azurerm_app_service" "group4-app" {
+  name                    = "group4-app"
+  location                = "Australia Central"
   resource_group_name     = azurerm_resource_group.RG_Group4_week3_20220321.name
-  app_service_plan_id     = azurerm_app_service_plan.Plan_Group4.id
+  app_service_plan_id     = azurerm_app_service_plan.group4-plan.id
   #https_only              = true
   #client_affinity_enabled = false
   tags                		= var.tags
 
  # site_config {
-      
-        #     + site_config {
-        #   + always_on                   = (known after apply)
-        #   + app_command_line            = (known after apply)
-        #   + auto_swap_slot_name         = (known after apply)
-        #   + default_documents           = (known after apply)
-        #   + dotnet_framework_version    = (known after apply)
-        #   + ftps_state                  = (known after apply)
-        #   + health_check_path           = (known after apply)
-        #   + http2_enabled               = (known after apply)
-        #   + ip_restriction              = (known after apply)
-        #   + java_container              = (known after apply)
-        #   + java_container_version      = (known after apply)
-        #   + java_version                = (known after apply)
-        #   + linux_fx_version            = (known after apply)
-        #   + local_mysql_enabled         = (known after apply)
-        #   + managed_pipeline_mode       = (known after apply)
-        #   + min_tls_version             = (known after apply)
-        #   + number_of_workers           = (known after apply)
-        #   + php_version                 = (known after apply)
-        #   + python_version              = (known after apply)
-        #   + remote_debugging_enabled    = (known after apply)
-        #   + remote_debugging_version    = (known after apply)
-        #   + scm_ip_restriction          = (known after apply)
-        #   + scm_type                    = (known after apply)
-        #   + scm_use_main_ip_restriction = (known after apply)
-        #   + use_32_bit_worker_process   = (known after apply)
-        #   + websockets_enabled          = (known after apply)
-        #   + windows_fx_version          = (known after apply)
-
-        #   + cors {
-        #       + allowed_origins     = (known after apply)
-        #       + support_credentials = (known after apply)
-        #     }
-        # }
+    
      
  #   }
 
