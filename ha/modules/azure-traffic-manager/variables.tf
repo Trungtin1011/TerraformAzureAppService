537a789ba4669e3d3280021258ffb608640cc42e
Trung Tin Ngo <54236859+Trungtin1011@users.noreply.github.com>
2022-03-26 12:01:35 +0700

    Delete ha directory

--- ha/modules/azure-traffic-manager/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group to deploy the traffic manager into"
-}
-
-variable "profile_name" {
-  type        = string
-  description = "The name of the traffic manager profile"
-}
