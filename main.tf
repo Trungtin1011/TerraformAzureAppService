6396907a8b8675738071075d1ecb936d7ecceaff
Trungtin1011 <54236859+Trungtin1011@users.noreply.github.com>
2022-03-26 14:53:45 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,13 +60,13 @@ resource "azurerm_mysql_server" "cs3-primary-sql" {
   storage_mb = 5120 #smallest
   version    = "5.7"
 
-  auto_grow_enabled                 = false
+  auto_grow_enabled                 = true
   backup_retention_days             = 7
-  geo_redundant_backup_enabled      = true
-  infrastructure_encryption_enabled = true
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
-  ssl_enforcement_enabled           = false
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_1"
 }
 
 # Primary Auto Scale
